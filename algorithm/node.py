16ebee11842644641db7b13c539f6e0aa503c7d2
matthewsokoloff <sokoloff.matthew27@gmail.com>
2026-02-10 12:55:09 -0500

    updated readability of node.py

--- algorithm/node.py
+++ algorithm/node.py
@@ -1,28 +1,26 @@
 import math
 
 class ISMCTSNode:
-    def __init__(self, parent=None, move=None):
+    def __init__(self, parent = None, move = None):
         self.parent = parent
-        self.move = move # Card that led to this node
+        self.move = move # the card that let to this node
         self.children = []
         self.visits = 0
         self.wins = 0
 
-    def uct(self, total_visits, c=1.41):
+    def uct(self, total_visits, c = 1.41):
         if self.visits == 0:
             return float("inf")
-        return (self.wins / self.visits) + c * math.sqrt(
-            math.log(total_visits) / self.visits
-        )
+        return (self.wins / self.visits) + c * math.sqrt(math.log(total_visits) / self.visits)
 
     def best_child(self):
-        return max(self.children, key=lambda c: c.visits)
+        return max(self.children, key = lambda c: c.visits) # returns the child with the largest visit count
 
     def untried_moves(self, legal_moves):
         tried = {child.move for child in self.children}
         return [m for m in legal_moves if m not in tried]
 
     def add_child(self, move):
-        child = ISMCTSNode(parent=self, move=move)
+        child = ISMCTSNode(parent = self, move = move)
         self.children.append(child)
         return child
